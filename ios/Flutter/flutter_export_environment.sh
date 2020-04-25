25da8a35eee649713ff9857aa91f4351e091c4f3
flp46 <alarconnieto@gmail.com>
2020-04-25 18:56:30 -0500

    Congifuracion de firebase para flutter, rename del proyecto, conexion de firebase, dependencias necesarias

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/flp46/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/flp46/Documentos/Ing_soft/Flutter/Flutter_avanzado/Curso-de-Flutter-Avanzado-en-Platzi"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/flp46/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
