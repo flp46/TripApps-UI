b8d54fe818190a90b41e9ed3c8f45a45f5d40042
flp46 <alarconnieto@gmail.com>
2020-04-26 19:48:43 -0500

    registro con google y  manejo de sesion

--- lib/User/ui/screens/sign_in_screen.dart
+++ lib/User/ui/screens/sign_in_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/bloc/user_bloc.dart';
+import 'package:platzi_trips_app/platzi_trips_cupertino.dart';
 import 'package:platzi_trips_app/widgets/button_green.dart';
 import 'package:platzi_trips_app/widgets/gradient_back.dart';
 
@@ -12,10 +15,28 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreen extends State<SignInScreen>{
+  
+  UserBloc userBloc;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return signInGoogleUI();
+
+    userBloc = BlocProvider.of(context);
+    return _handleSessionStatus();
+  }
+
+  Widget _handleSessionStatus(){
+    return StreamBuilder(
+      stream: userBloc.authStatus,
+      builder: (BuildContext context, AsyncSnapshot snapshot){
+        if (!snapshot.hasData || snapshot.hasError){
+          return signInGoogleUI();
+        } else {
+          return PlatziTripsCupertino();
+        }
+      }
+    );
   }
 
   Widget signInGoogleUI(){
@@ -39,7 +60,7 @@ class _SignInScreen extends State<SignInScreen>{
               ButtonGreen(
                 text: 'Login with Gmail', 
                 onPressed: (){
-
+                  userBloc.signIn().then((user) => print('El usuario es ${user.displayName}'));
                 },
                 width: 300.0,
                 height: 50.0,
