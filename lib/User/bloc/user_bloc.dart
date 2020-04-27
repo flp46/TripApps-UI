25c60cbec069ba0907134054c0cdadf61699ff73
flp46 <alarconnieto@gmail.com>
2020-04-26 19:48:43 -0500

    registro con google y  manejo de sesion

--- lib/User/bloc/user_bloc.dart
+++ lib/User/bloc/user_bloc.dart
@@ -1,6 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/repository/auth_repository.dart';
 
 class UserBloc implements Bloc{
+
+  final authRepository = AuthRepository();
+
+  Future<FirebaseUser> signIn() => authRepository.signInFirebase();
+
+  Stream<FirebaseUser> streamFirebase = FirebaseAuth.instance.onAuthStateChanged;
+  Stream<FirebaseUser> get authStatus => streamFirebase; 
+
   @override
   void dispose() {
     // TODO: implement dispose
---
+++ lib/User/repository/firebase_auth_api.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class FirebaseAuthAPI {
+
+  //Inicializar las clases FirebaseAuth y GoogleSignIn
+  final FirebaseAuth _auth = FirebaseAuth.instance; 
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  // Ahora puedo usar Firebase _auth para autenticar en mi codigo de Dart
+  Future<FirebaseUser> handleSignIn() async { //Como resultado de la autenticacion Firebase retornar un objeto de tipo FirebaseUser, eso es lo que esta prometido que a futuro voy a recibir. Como es a futuro es async
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn(); //Levanta el pop-up de google para que se autentique y espera respuesta
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication; //Obtiene y guarda el token de autenticacion de google
+
+    //Voy a guardar en la variable de tipo FirebaseUser user, lo que me traiga el metodo de registro 
+    //con credenciales de FirebaseAuth. Ese metodo de Firebase signIntWithCredencials me pide que le pase las credenciales,
+    //esas las voy a sacar del token que viene en la autenticacion en el paso anterior
+    FirebaseUser user = await _auth.signInWithCredential(GoogleAuthProvider.getCredential(idToken: googleAuth.idToken, accessToken: googleAuth.accessToken));
+
+    //Lo que me retorna el futre, sera ese user
+    return user;
+  }
+}
